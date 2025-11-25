c1549e404d3084f8a3182d7784a52298639f99d8
AliciaTong <aliciatong227@gmail.com>
2025-11-25 12:00:09 -0800

    add file

--- work-dataanalysis.py
+++ work-dataanalysis.py
@@ -7,18 +7,17 @@ def main():
     path = "data/NYC_Central_Park_weather_1869-2022.csv"
     file = open(path)
     file.readline()
-    total_temp = 0
     year = int(1869)
     for line in file:
         info = line.split(",")
         date = info[0].split("-")
 
         if int(date[0]) == year and date[1] == "06":
+            total_temp = 0
             if info[5].strip():
                 average = float(info[5])
                 total_temp += average
             print(f"{year} {total_temp} deg")
-            total_temp = 0
             year += 1
 
 
