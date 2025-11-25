0150a2bcc50792b463d3be20daeb29afbbf474a6
AliciaTong <aliciatong227@gmail.com>
2025-11-25 11:51:31 -0800

    dataaaaaa

---
+++ work-dataanalysis.py
@@ -0,0 +1,26 @@
+# Data Analysis
+# Author: Alicia
+# Analyse the data provided in class
+
+
+def main():
+    path = "data/NYC_Central_Park_weather_1869-2022.csv"
+    file = open(path)
+    file.readline()
+    total_temp = 0
+    year = int(1869)
+    for line in file:
+        info = line.split(",")
+        date = info[0].split("-")
+
+        if int(date[0]) == year and date[1] == "06":
+            if info[5].strip():
+                average = float(info[5])
+                total_temp += average
+            print(f"{year} {total_temp} deg")
+            total_temp = 0
+            year += 1
+
+
+if __name__ == "__main__":
+    main()
